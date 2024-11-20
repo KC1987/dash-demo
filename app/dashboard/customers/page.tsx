923eb3eab4520921412fc10bb9de437934302553
KC87 <kaspars.cernovskis@gmail.com>
2024-11-20 07:28:50 +0200

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page () {
+    return (
+        <div>
+            <h2>Customers</h2>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page () {
+    return (
+        <div>
+            <h2>Invoices</h2>
+        </div>
+    )
+}
